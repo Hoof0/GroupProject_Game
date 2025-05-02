f6d206b522c1facea749a6662e13a344cfe208ae
Hoof0 <hho2k5@gmail.com>
2025-05-02 09:06:01 +0700

    I made stuff
    testing

--- GameGroup_project/test.cpp
+++ GameGroup_project/test.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 
 int main() {
-    evidence e1("Key", "A small key that opens a door.");
-    evidence e2("Note", "A note with a clue.");
-    evidence e3("GAY");
-    string a = e1.getName();
-    string b = e2.getName();
-    string result;
-    cin >> a;
-    cin >> b;
-    if (a == e1.getName() && b == e2.getName()) {
-        cout << "You found a connection" << endl;
-        result = e3.getName();
-    }
+    evidence* e1 = new evidence;
+    evidence* e2 = new evidence;
+    evidence* e3 = new evidence;
+    e1->setName("Bob");
+    e2->setName("Bob like men");
+    string a = e1->getName();
+    string b = e2->getName();
+    string result = "Bob is gay";
 
-    cout << result << endl;
+    if (a == e1->getName() && b == e2->getName()) {
+        cout << "You found a connection" << endl;
+        cout << result;
+    }
+    delete e1;
+    delete e2;
+    delete e3;
     return 0;
 }
