49ee1f3c971e8bf128034e7fb848cf8e1b57c562
annae-roseweed <celpxrosewood@gmail.com>
2025-05-21 09:27:28 +0700

    npc + barebone of diaOp /annae

---
+++ GameGroup_project/NPC.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <StoryElement.h>
+#include <vector>
+#include <Story.h>
+using namespace std;
+
+class NPC:public StoryElement
+{
+    private:
+    string name;
+    bool isSuspect;
+    vector<string> dialogue;
+    public:
+
+    NPC() = default;
+    NPC(const string& desc, const string& NPCname, bool suspect = false, string dialogue):StoryElement(desc), name(NPCname), isSuspect(suspect) {};
+    
+    string getName() const;
+    bool getIsSuspect() const;
+    vector<string> getDialogues() const;
+    string getDialogue() const;
+
+    void setName(const string &NPCname);
+    void setSuspect(bool suspect);
+    void addDialogue(const string &dialogue);
+};
---
+++ GameGroup_project/dialogueOption.h
@@ -0,0 +1,8 @@
+#include <string>
+#include "StoryElement.h"
+#include <vector>
+#include "Story.h"
+#include "NPC.h"
+using namespace std;
+
+class dialogueOption:public NPC
