8df0bf2558b1f34e8723b7fb0636f53c1d9d73dc
Hoof0 <hho2k5@gmail.com>
2025-05-22 10:00:58 +0700

    slowly print text // hoof

--- GameGroup_project/StoryElement.cpp
+++ GameGroup_project/StoryElement.cpp
@@ -3,6 +3,11 @@
 
 
 string StoryElement::getDescription() const{
+    for (char c : description){
+        cout << c << flush;
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+    cout << endl;
     return description;
 };
 
