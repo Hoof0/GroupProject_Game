59d25b78d95a21a40f923af51f8c308303455ca5
Hoof0 <hho2k5@gmail.com>
2025-05-25 21:27:02 +0700

    I give it up

--- GameGroup_project/StoryElement.cpp
+++ GameGroup_project/StoryElement.cpp
@@ -1,6 +1,6 @@
 #include "StoryElement.h"
 
-int speed = 5;
+
 
 string StoryElement::getDescription() const{
     return description;
@@ -11,57 +11,16 @@ void StoryElement::setDescription(string dsc){
 };
 
 void StoryElement::printDescription() const{
-    const int width = 80; // Adjust this based on your terminal width
+    const int LineLength = 80; // Set the line length for text wrapping
     string word = "";
     int currentLineLength = 0;
-    
-    for (size_t i = 0; i < description.length(); i++) {
-        char c = description[i];
-        
-        if (c == ' ' ||  c == '\n' || i == description.length() - 1) {
 
-            if (i == description.length() - 1 && c != ' ' && c != '\t' && c != '\n') {
-                word += c;
-            }
-            
-            
-            if (currentLineLength + word.length() > width && currentLineLength > 0) {
-                cout << endl; 
-                currentLineLength = 0;
-            }
-            
-          
-            for (char wc : word) {
-                cout << wc << flush;
-                this_thread::sleep_for(chrono::milliseconds(speed));
-                currentLineLength++;
-            }
-            
-         
-            if (c == '\n') {
-                cout << c << flush;
-                this_thread::sleep_for(chrono::milliseconds(speed));
-                currentLineLength = 0;
-            } else if (c == ' ' || c == '\t') {
-               
-                if (currentLineLength > 0) {
-                    cout << c << flush;
-                    this_thread::sleep_for(chrono::milliseconds(speed));
-                    currentLineLength++;
-                }
-            }
-            
-            word = ""; 
-        } else {
-            word += c; 
-        }
     // Print text slowly
+
     for (char c : description){
         std::string description;
         std::cout << c << std::flush;
-        this_thread::sleep_for(chrono::milliseconds(5));
+        this_thread::sleep_for(chrono::milliseconds(1));
     }
-    
-    cout << endl;
-}
+    std::cout << std::endl;
 };
